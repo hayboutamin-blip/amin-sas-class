b4309462e9418a59773709658b6bba948a50a00a
amin <hayboutamin@gmail.com>
2025-09-02 18:00:31 +0200

    first commit in Day_1

---
+++ challenge 8.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int  main()
+{
+    float a,b,c;
+    float M_g;
+
+    printf("entrer 1er nombre: ");
+    scanf("%f",&a);
+    printf("entrer 2er nombre: ");
+    scanf("%f",&b);
+    printf("entrer 3er nombre: ");
+    scanf("%f",&b);
+
+    M_g = pow(a * b * c,3);
+
+    printf("le Moyenne est: ",M_g);
+}
